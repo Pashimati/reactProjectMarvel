9d188e90c37f6883fc30912f1a902f8872253804
Pashimati <Pashimatii@gmail.com>
2022-07-12 09:21:49 +0300

    create MarvelService

---
+++ src/services/MarvelService.js
@@ -0,0 +1,28 @@
+
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=ba8c821c6d29b60d2e73c834365677cf';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+        }
+
+        return await res.json()
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+
+    }
+}
+
+export default MarvelService;
