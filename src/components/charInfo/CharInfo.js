e632ec03703de68b855aebebaacb6c59a9659485
Pashimati <Pashimatii@gmail.com>
2022-07-17 12:23:58 +0300

    add ErrorBoundary and fix

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import {Component} from "react";
+import PropTypes from "prop-types"
 import './charInfo.scss';
 import Spinner from "../spinner/spinner";
 import ErrorMessage from "../errorMessage/errorMessage";
@@ -114,6 +115,10 @@ const View = ({char}) => {
             </ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
 }
 
 export default CharInfo;
