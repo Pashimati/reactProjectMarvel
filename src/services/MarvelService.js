10178f2a8903bcbdbcaf69f8a8d7b97e87cca5d3
Pashimati <Pashimatii@gmail.com>
2022-07-31 16:00:15 +0300

    create method getCharacter by name

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -52,7 +52,12 @@ const  useMarvelService = () => {
         }
     }
 
-    return {loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic}
+    const getCharacterByName = async (name) => {
+        const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+        return res.data.results.map(_transformCharacter);
+    }
+
+    return {loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic, getCharacterByName}
 }
 
 export default useMarvelService;
